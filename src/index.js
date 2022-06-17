01b0257397cff4462d6e5871b20f3af357ebddc0
margaret manyeki <maggiemanyeki6@gmail.com>
2022-06-17 14:04:23 +0300

    commit tree 86ed298065e3b585a9eba36d102151835cc4c626

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+ fetch("http://localhost:3000/dogs/:id",{
+     method:"POST",
+     headers:{
+        "Content-Type": "application/json",
+        Accept: "application/json",
+     },
+     body: JSON.stringify({
+         dog:"name",dog:"breed",dog:"sex",dog:"editing dog",
+     })
+});
 })
